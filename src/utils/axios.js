580d90b95a1c24212dd028367ffa5b0d7791ed50
ajmal <ajmalbinzali@gmail.com>
2024-12-21 16:14:39 +0530

    "url changed"

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosUrl =  axios.create({
-    baseURL:"http://localhost:3000/",
+    baseURL:"https://adhaarocrserver.onrender.com/",
     withCredentials:true
 });
 
